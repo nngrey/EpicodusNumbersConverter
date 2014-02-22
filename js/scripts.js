35e3053f93e679355c07cd11f5b23a101d855000
Nathan Grey <greynng@gmail.com>
2014-02-22 13:48:32 -0800

    add convert numbers greater than 100

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,29 @@
 var numbers = function(input) {
-  var objectOnes = {};
-  objectOnes[1] = "one";
-  var objectTens = {};
-  objectTens[20] = "twenty";
-  if (input === 1) {
-    return objectOnes[1];
+  var objectOnes = {
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine"
+  };
+  var objectTens = {
+    20: "twenty" 
+  };
+  var objectHundreds = {
+    100: "one-hundred"
+  }
+//  return objectOnes[input];
+  if (input < 20) {
+    return objectOnes[input];
   } else if (input > 20) {
     var ones = input % 10;
-    var tens = input - ones;
-    return objectTens[tens] + " " + objectOnes[ones];
+    var tens = input -100 - ones;
+    var hundreds = input - tens - ones;
+    return  objectHundreds[hundreds] + " " + objectTens[tens] + " " + objectOnes[ones];
   }
+
 };
--- spec/spec.js
+++ spec/spec.js
@@ -1,10 +1,16 @@
 describe ("numbers", function(){
   it('will take the number 1 and return "one"', function(){
-    numbers(1).should.equal("one");
+    numbers(2).should.equal("two");
   });
 
-  it('will take two digit numbers and return the appropriate written words', function(){
-    numbers(21).should.equal("twenty one");
+  it('will take a singel digit number and return the appropriate written word', function(){
+    numbers(3).should.equal("three");
   });
+  it ('will take a number between 20 and 30 and return the appropriate written words', function(){
+  	numbers(24).should.equal("twenty four");
+  });
+  it ('will take 125 and return "one-hundred twenty five"', function(){
+  	numbers(125).should.equal("one-hundred twenty five");
+	});
 });
 
