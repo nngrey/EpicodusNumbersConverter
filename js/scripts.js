f80cf648fd5b1d7dc0113183403a21e259223fc4
Nathan Grey <greynng@gmail.com>
2014-02-23 14:56:12 -0800

    add functionality up to 999,000

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,85 @@
 var numbers = function(input) {
+  var result = "";
   var objectOnes = {
-    1: "one",
-    2: "two",
-    3: "three",
-    4: "four",
-    5: "five",
-    6: "six",
-    7: "seven",
-    8: "eight",
-    9: "nine"
+    0: "", 1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine", 10: "ten", 11: "eleven", 12: "twelve", 13: "thirteen", 14: "fourteen", 15: "fifteen", 16: "sixteen", 17: "seventeen", 18: "eighteen", 19: "nineteen"
   };
   var objectTens = {
-    20: "twenty" 
+    1: "", 2: "twenty", 3: "thirty", 4: "forty", 5: "fifty", 6: "sixty", 7: "seventy", 8: "eighty", 9: "ninety" 
   };
-  var objectHundreds = {
-    100: "one-hundred"
-  }
-//  return objectOnes[input];
-  if (input < 20) {
-    return objectOnes[input];
-  } else if (input > 20) {
-    var ones = input % 10;
-    var tens = input -100 - ones;
-    var hundreds = input - tens - ones;
-    return  objectHundreds[hundreds] + " " + objectTens[tens] + " " + objectOnes[ones];
-  }
+
+switch (true)
+{
+  case (input >= 100000):
+    result = objectOnes[Math.floor(input/100000)] + " hundred";
+  //  console.log(result);
+    if (input % 100000 > 0) {
+      result = result + " ";
+    } else {
+      result = result + " thousand";
+      break;
+    }
+    input = input % 100000;
+
+  case (input >= 20000):
+    result = result + objectTens[Math.floor(input / 10000)];
+    console.log(result);
+    if (input % 10000 >0) {
+      result = result + "-";
+    } else {
+      break;
+    }
+    input = input % 10000;
+
+  case (input >= 1000):
+    result = result + objectOnes[Math.floor(input / 1000)] + " thousand";
+    if (input % 1000 > 0) {
+      result = result + " ";
+    } else {
+      break;
+    }
+    input = input % 1000;
+
+  case (input >= 100):
+    result = result + objectOnes[Math.floor(input / 100)] + " hundred";
+    if (input % 100 > 0){
+      result = result + " ";
+    } else {
+      break;
+    }
+    input = input % 100;
+    if (input < 20) {
+      result = result + objectOnes[input];
+      break;
+    }
+
+  case (input >= 20):
+    result = result + objectTens[Math.floor(input / 10)];
+    if (input % 10 > 0) {
+      result = result + "-";
+    }
+    input = input % 10;
+
+  default:
+    result = result + objectOnes[input];
+    break;
+}
+
+return result;
 
 };
+
+
+
+
+  // if (input >= 100) {
+  //   result = objectHundreds[Math.floor(input / 100)];
+  //   input = input % 100;
+
+  // } else if (input > 20) {
+  //   result = result + objectTens[Math.floor(input / 10)];
+  //   input = input % 10;
+
+  // } else {
+  //   result = result + objectOnes[input];
+  // }
+  // return result;
--- spec/spec.js
+++ spec/spec.js
@@ -1,16 +1,56 @@
 describe ("numbers", function(){
+
   it('will take the number 1 and return "one"', function(){
     numbers(2).should.equal("two");
   });
 
-  it('will take a singel digit number and return the appropriate written word', function(){
+  it('will take a single digit number and return the corresponding word', function(){
     numbers(3).should.equal("three");
   });
-  it ('will take a number between 20 and 30 and return the appropriate written words', function(){
-  	numbers(24).should.equal("twenty four");
+
+  it ('will take 20 and return "twenty"', function(){
+  	numbers(20).should.equal("twenty");
   });
-  it ('will take 125 and return "one-hundred twenty five"', function(){
-  	numbers(125).should.equal("one-hundred twenty five");
+
+  it ('will take a two-digit number and return the corresponding words', function(){
+  	numbers(24).should.equal("twenty-four");
+  });
+
+  it ('will take 100 and return "one hundred"', function(){
+  	numbers(100).should.equal("one hundred");
 	});
+
+	it ('will take 118 and return "one hundred eighteen"', function(){
+		numbers(118).should.equal("one hundred eighteen");
+	});
+
+	it ('will take a three-digit number and return the corresponding words', function(){
+		numbers(125).should.equal("one hundred twenty-five");
+	});
+
+	it ('will take 1000 and return "one thousand"', function(){
+		numbers(1000).should.equal("one thousand");
+	});
+
+	it ('will take a four-digit number and return the corresponding words', function(){
+		numbers(2345).should.equal("two thousand three hundred forty-five");
+	});
+
+	it ('will take 10000 and return ten thousand', function (){
+		numbers(10000).should.equal("ten thousand");
+	});
+
+	it('will take a five-digit number and return the corresponding words', function(){
+		numbers(23567).should.equal("twenty-three thousand five hundred sixty-seven");
+	});
+
+	it ('will take 100000 and return "one hundred thousand', function(){
+		numbers(100000).should.equal("one hundred thousand");
+	});
+
+	it ('will take a six-digit number and return the corresponding words', function(){
+		numbers(123217).should.equal("one hundred twenty-three thousand two hundred seventeen");
+	});
+
 });
 
